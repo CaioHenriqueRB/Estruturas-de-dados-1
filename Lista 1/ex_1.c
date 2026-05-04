8cdd858e9b1a03bbf0a70d5053a6579e0be93373
Caio Barreto <henrique.caio18@gmail.com>
2026-05-04 15:59:51 -0300

    Ex1 - incompleto

--- Lista 1/ex_1.c
+++ Lista 1/ex_1.c
@@ -1,6 +1,21 @@
+/*1) Implemente um TAD contaBancaria, com os dados: número da conta, nome do titular
+e saldo. Os clientes podem fazer as seguintes operações:
+a) Iniciar um conta, para isso precisam ter um saldo inicial;
+b) Depositar um valor;
+c) Sacar um valor;
+d) Imprimir o saldo.*/
+
 #include <stdio.h>
 #include <string.h>
 
+typedef struct {
+    int num_conta;
+    char nome[50];
+    float saldo;
+} ContaBacaria;
+
+
+
 int main(){
-    printf("First Commit Push");
+    
 }
